80f10e3bc78bf264f666751216205d55640a363f
Crowdbotics <team@crowdbotics.com>
2022-08-09 21:56:39 +0000

    Initial commit.

---
+++ hulu_web_36695/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for hulu_web_36695 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hulu_web_36695.settings')
+
+application = get_wsgi_application()
